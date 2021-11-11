bef8fd6b7c651e2ff4649828692e4db0939993a0
Harmony Simon-Duchatel <sd.harmony@yahoo.fr>
2021-11-11 19:10:34 +0100

    correctif vibreur et notification + couleurs + design app

--- app/src/main/java/m1/pmob/veget_eau/AjouterFragment.kt
+++ app/src/main/java/m1/pmob/veget_eau/AjouterFragment.kt
@@ -3,6 +3,7 @@ package m1.pmob.veget_eau
 import android.content.Context
 import android.os.Bundle
 import android.os.VibrationEffect
+import android.os.VibrationEffect.DEFAULT_AMPLITUDE
 import android.os.Vibrator
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -10,6 +11,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.ViewModelProvider
 import m1.pmob.veget_eau.databinding.FragmentAjouterBinding
 
@@ -28,11 +30,17 @@ class AjouterFragment : Fragment(R.layout.fragment_ajouter) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentAjouterBinding.bind(view)
-        model.setfragment(this)
+       // model.setfragment(this)
         binding.bAjouter.setOnClickListener{
             val nc = binding.edNomverna.text.toString().trim()
             val ns = binding.edNomscient.text.toString().trim()
             val uri = binding.edUri.text.toString().trim()
+            if(nc=="" || ns==""){
+                val vibrator = context?.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+                vibrator.vibrate(VibrationEffect.createOneShot(50,DEFAULT_AMPLITUDE))
+                afficherDialog("mettre au moins un nom :(")
+                return@setOnClickListener
+            }
             model.addPlantes(n=nc,ns=ns,uri=uri)
             binding.edNomscient.text.clear()
             binding.edNomverna.text.clear()
@@ -41,8 +49,14 @@ class AjouterFragment : Fragment(R.layout.fragment_ajouter) {
 
         }
     }
-    fun vibrate(){val vibrator =context?.applicationContext?.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-        vibrator.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE))
+    fun afficherDialog( s: String ){
+        context?.let {
+            AlertDialog.Builder(it)
+                .setMessage( s )
+                .setPositiveButton("OK"){ d, _ -> d.dismiss() }
+                .show()
+        }
+        return
     }
 
 }
--- app/src/main/java/m1/pmob/veget_eau/MyViewModel.kt
+++ app/src/main/java/m1/pmob/veget_eau/MyViewModel.kt
@@ -13,30 +13,9 @@ class MyViewModel(application: Application): AndroidViewModel(application) {
     //pour recherche plabtes avec prefixes
     var certainesPlantes = MutableLiveData<List<Eplante>>()
 
-
-    //TODO il faudra sûrement avoir à changer le type de AjouterFragment vers une interface ou quoi ?
-    var fragment: AjouterFragment? = null // le fragment doit s'auto enregistrer pour qu'on puisse accéder à son contexte
-
-    fun setfragment(frag:AjouterFragment){this.fragment = frag
-    }
-
-    fun callvibrate(){
-
-        if((this.fragment?.lifecycle?.currentState?.isAtLeast(Lifecycle.State.DESTROYED))== false  ){
-            this.fragment?.vibrate()
-        }
-    }
     fun addPlantes(n: String?, ns: String?, uri: String?){
         Thread{
-            if(n!="" || ns!=""){
-                //Log.d("getPlante", "enregistré)
-                dao.ajoutPlante(Eplante(nomverna = n?.trim(),nomscient = ns?.trim(), uri = uri?.trim()))
-            }else{
-                //Log.d("getPlante", "non enregistré")
-                //val vibrator = context?.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-                //vibrator.vibrate(VibrationEffect.createOneShot(50,DEFAULT_AMPLITUDE))
-            callvibrate()
-            }
+            dao.ajoutPlante(Eplante(nomverna = n?.trim(),nomscient = ns?.trim(), uri = uri?.trim()))
         }.start()
     }
 
