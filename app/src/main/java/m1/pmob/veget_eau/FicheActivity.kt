ab0aa0e80d39f1eef9d02b74713d2cfa7ca79050
Harmony Simon-Duchatel <sd.harmony@yahoo.fr>
2021-11-24 22:05:42 +0100

    ouvre nouvelle activite quand touche sur element de la liste

---
+++ app/src/main/java/m1/pmob/veget_eau/FicheActivity.kt
@@ -0,0 +1,11 @@
+package m1.pmob.veget_eau
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FicheActivity: AppCompatActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
