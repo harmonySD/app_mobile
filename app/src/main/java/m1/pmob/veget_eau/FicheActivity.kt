9e8b185338c1e3f231b756ee9d9a3abeb6b9af7e
Harmony Simon-Duchatel <sd.harmony@yahoo.fr>
2021-11-25 15:39:45 +0100

    ajout fragment pour ficheplante

--- app/src/main/java/m1/pmob/veget_eau/FicheActivity.kt
+++ app/src/main/java/m1/pmob/veget_eau/FicheActivity.kt
@@ -2,10 +2,31 @@ package m1.pmob.veget_eau
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayoutMediator
+import m1.pmob.veget_eau.databinding.ActivityMainBinding
+import m1.pmob.veget_eau.databinding.FicheActivityBinding
 
 class FicheActivity: AppCompatActivity()  {
+    val binding : FicheActivityBinding by lazy{ FicheActivityBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val names = listOf("FICHE", "MODIFIER")
+        //creation des fragments
+        val ficheFragment = FicheFragment.newInstance()
+        val modifierFragment = ModifierFragment.newInstance()
+        //le slide
+        val pagerAdapter = ScreenSlidePagerAdapter(
+            this,
+            mutableListOf<Fragment>(ficheFragment,modifierFragment)
+        )
+        binding.pager.adapter=pagerAdapter
+
+        setContentView(binding.root)
+
+        TabLayoutMediator(binding.tabLayout,binding.pager) {tab,position -> tab.text=names[position]}
+            .attach()
     }
+
 }
