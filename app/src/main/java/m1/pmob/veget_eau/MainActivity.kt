ee88136b916ce90ae2aaff062783eee3fe4daf42
Alexandre SABRI <alexandre.sabri@u-paris.fr>
2021-12-26 23:20:14 +0100

    strings.xml ajout de 4 chaines de caractères dont il est préférables qu'elles soient bien unifiées dans l'application. 3 ont rapport avec la création d'un canal de notification 1 a rapport avec la création d'un travail journalier pour vérifier la bd périodiquement.
    PlanningWorker.kt
    logique normalement complète
    dans cet ordre
    - vérifie la bd si des arrosages sont à faire
    - la met à jour
    - s'assure que le travail sera effectué le lendemain
    - lance une notification
    
    build.gradle
    ajouts de divers imports pour pouvoir faire le Worker
    
    MainActivity.kt
    création d'un canal de notification pour piquer l'utilisateur si nécessité arrosage
     vérification qu'un travail d'arrosage sera fait chaque jour
    
    AndroidManifest.xml
    ajout d'une ligne pour pouvoir envoyer des notifications

--- app/src/main/java/m1/pmob/veget_eau/MainActivity.kt
+++ app/src/main/java/m1/pmob/veget_eau/MainActivity.kt
@@ -1,5 +1,9 @@
 package m1.pmob.veget_eau
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -27,5 +31,29 @@ class MainActivity : AppCompatActivity() {
 
         TabLayoutMediator(binding.tabLayout,binding.pager) {tab,position -> tab.text=names[position]}
             .attach()
+        // on s'assure que chaque jour une vérif est faite de la bd et indique les plantes à arroser
+        PlanningWorker.schedule_work(applicationContext)
+    }
+    /*
+    CE BOUT DE CODE EST _TRES_ INSPIRE DU SITE OFFICIEL D'ANDROID
+    https://developer.android.com/training/notify-user/build-notification#kts
+    */
+
+    private fun createNotificationChannel() {
+        // on doit créer un channel de notification pour envoyer des notifications
+        // si on en possède déjà un ce n'est pas un problème (cf. docu notifications)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val id = getString(R.string.notification_channel_id)
+            val name = getString(R.string.notification_channel_name)
+            val descriptionText = getString(R.string.notification_channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(id, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
--- app/src/main/java/m1/pmob/veget_eau/PlanningWorker.kt
+++ app/src/main/java/m1/pmob/veget_eau/PlanningWorker.kt
@@ -1,16 +1,15 @@
 package m1.pmob.veget_eau
 
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.icu.text.DateFormat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import java.util.Date
 import androidx.core.util.rangeTo
 import androidx.lifecycle.ViewModelProvider
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.PeriodicWorkRequestBuilder
-
-import androidx.work.Worker
-
-import androidx.work.WorkerParameters
+import androidx.work.*
 
 
 import java.util.*
@@ -18,25 +17,40 @@ import java.util.concurrent.TimeUnit
 
 class PlanningWorker(appContext: Context, workerParams: WorkerParameters):
     Worker(appContext, workerParams) {
+    companion object{
+        fun schedule_work(cntxt:Context){
+            // cette fonction sert à s'assurer qu'un travail est prévu tout les jours pour vérifier la bd
+            // elle essaie simplement d'en ajouter un et si il existe déjà il ne se passera rien.
+            // on créé un constructeur pour le travail à réaliser
+            val workReqBuilder = PeriodicWorkRequestBuilder<PlanningWorker>(2,TimeUnit.MINUTES) // (1,TimeUnit.DAYS)
+            WorkManager.getInstance(cntxt).enqueueUniquePeriodicWork(// on envoie notre travail au système
+                cntxt.getString(R.string.WorkRequestID), // un id pour qu' on ai au maxium un seul travail.
+                ExistingPeriodicWorkPolicy.KEEP, // il doit normalement déjà exister, auquel cas on ne fait rien.
+                workReqBuilder.build() // le travail
+            )
+        }
+    }
+
+
     override fun doWork(): Result {
+        // override obligatoire,
+        // là où se fait tout le travail
+
        val dao =  PlantsDatabase.getDatabase(applicationContext).myDao()
         val lstarros : List<Earrosage> = dao.getArrosageToCheckWater()
         val lstidtowater:MutableList<Long> = MutableList(0) { 0 }
         val cbegin =Calendar.getInstance()
         val cend = Calendar.getInstance()
-        val ctest = Calendar.getInstance() // attention à ne pas modifier, il est réglé sur le temps actuel !
-        // pour savoir si la date d'aujourd'hui est dans l'intervalle des arrosages
-        // prendre le nombre de secondes du premier janvier  de l'année courante
-        // lui soustraire le nombre de secondes du premier janvier 2000 (toutes nos dates sont en 2000)
-        // on a maintenant le nombre de secondes séparant deux jours d'années // problème 29 février ?
-        // on soustrait ce nombre de secondes à la date d'aujourd'hui
-        // (mode normal) de là si date de début < date de fin
-        // (mode inversé) de là si date de début > date de fin
+        val ctest = Calendar.getInstance()
 
-        // (mode normal)  il faut arroser la plante si on se trouve DANS l'intervalle  date début <= (date actuelle - x) <= date fin
-            // ET datedebut mod 86400*freqarrosage == dateactuelle -x mod 86400*freqarrosage
-       //   il faut arroser la plante si on ne se trouve PAS l'intervalle  date début (date actuelle - x) =< date fin
-            // ET datefin mod 86400*freqarrosage == dateactuelle -x mod 86400*freqarrosage
+        // Pour savoir si aujourd'hui est un jour où il faut arroser
+        // (mode normal)   date de début < date de fin
+        // (mode inversé)  date de début > date de fin
+
+        // (mode normal)  il faut arroser la plante si on se trouve DANS l'intervalle  date début <= (date actuelle ) <= date fin
+            // ET  numerojour(datedebut) mod freqarrosage == numerojour(dateactuelle) mod freqarrosage
+       // (mode inversé ) il faut arroser la plante si on ne se trouve PAS l'intervalle  date début <= date actuelle =< date fin
+            // ET numerojour(datefin) mod freqarrosage == nbjour(dateactuelle) mod freqarrosage
 
         for (e in lstarros){
             cbegin.timeInMillis=e.deb.time
@@ -47,29 +61,49 @@ class PlanningWorker(appContext: Context, workerParams: WorkerParameters):
                         // on est sur le bon jour de l'intervalle (rapport à la fréquence )
                         lstidtowater.add(e.idp)
                 }
-            }else{ // mode spécial cbegin > cend
-                if(( ctest !in cbegin..cend)&& // on est dans la période d'arrosage ET
+            }else{ // mode inversé cbegin > cend
+                if(( ctest !in cbegin..cend) && // on est dans la période d'arrosage ET
                     cend.get(Calendar.DAY_OF_YEAR)%e.interval ==ctest.get(Calendar.DAY_OF_YEAR)%e.interval){
                     // on est sur le bon jour de l'intervalle rapport à la fréquence
                     lstidtowater.add(e.idp)
                 }
             }
        }
-        // AJOUTER UNE VERIFICATION DE l'UNICITE DE CHAQUE ELEMENT DE LA LISTE
+
+        // TODO AJOUTER UNE VERIFICATION DE l'UNICITE DE CHAQUE ELEMENT DE LA LISTE ?
         // POUR REDUIRE LES OPERATIONS DANS LA BD
         for (id in lstidtowater){ // on met à jour la bd en indiquant  qu'il faut arroser toutes les plantes
             // dont aujourd'hui est un jour d'arrosage.
             dao.setWater(id,true)
         }
 
-        // schedule travail pour le lendemain
-        OneTimeWorkRequestBuilder<PlanningWorker>()
-        PeriodicWorkRequestBuilder<PlanningWorker>(1,TimeUnit.DAYS)
-        // lancer une notification pouvant être skippé
-            //option pour choisir l'heure du check / de la notif ?
+        // on s'assure qu'un travail est schedule pour le lendemain.
+        schedule_work(applicationContext)
+
+        //Construction d'une notification pouvant être skippée
+        //option pour choisir l'heure du check / de la notif ?
+        val notifBuilder = NotificationCompat.Builder(applicationContext,applicationContext.getString(R.string.notification_channel_id))
+        // .setSmallIcon(R.drawable.)sait pas quoi mettre comme icone de notif
+        notifBuilder.setContentTitle("Veget\'eau")
+        notifBuilder.setContentText("Vous avez des plantes à arroser !")
+        notifBuilder.setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        // la notification a besoin d'un intent pour savoir quoi faire si on clique dessus
+        val intent = Intent(applicationContext, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        // on enveloppe l'intent dans un pending pour pouvoir l'envoyer à travers le système Android
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(applicationContext, 0, intent, 0)
+        notifBuilder.setContentIntent(pendingIntent)  //on fournit au constructeur de notification le pending intent
+        notifBuilder.setAutoCancel(true)// on peut skipper la notification
+        with(NotificationManagerCompat.from(applicationContext)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(1, notifBuilder.build())
+        }
+
         // penser à rajouter un check à chaque lancement de l'application ????
         return Result.success()
     }
-
 
 }
